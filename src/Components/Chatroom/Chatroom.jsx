9cd01b3b25857a19ab2640ac1408e27e41be3cc6
Parth Tyagi <parthtyagicode@gmail.com>
2023-06-18 19:49:09 +0530

    chatroom completed

--- src/Components/Chatroom/Chatroom.jsx
+++ src/Components/Chatroom/Chatroom.jsx
@@ -1,28 +1,54 @@
 import React from 'react';
-import {TiLocationArrow} from 'react-icons/ti';
+import { TiLocationArrow } from 'react-icons/ti';
 
 
 
 const Chatroom = () => {
     return (
-        <div className='w-full h-full p-[1.5rem] flex flex-col bg-pink-400  '>
-            <div className="w-full p-[0.5rem] flex bg-yellow-400  " >
-                <div className="w-[50%] text-[1.3rem] font-[700] p-[1rem] rounded-[1rem] flex justify-center items-center bg-green-400  ">Messages( )</div>
-                <div className="w-[50%] text-[1.3rem] font-[700] p-[1rem] rounded-[1rem] flex justify-center items-center bg-green-400  ">Participants( )</div>
+        <div className='w-full h-full p-[1.5rem] flex flex-col rounded-[1rem] bg-gray-100  '>
+            <div className="w-full p-[0.5rem] flex rounded-[1rem] bg-white " >
+                <div className="w-[50%] text-[1.3rem] font-[700] p-[1rem] rounded-[1rem] flex justify-center items-center bg-green-100  ">Messages( )</div>
+                <div className="w-[50%] text-[1.3rem] font-[700] p-[1rem] rounded-[1rem] flex justify-center items-center  ">Participants( )</div>
             </div>
 
             <div className="w-full mt-[2rem] mb-[2rem]  relative ">
-                <span className='bg-gray-100 text-[1.2rem] font-[500] px-[2rem] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  '>Messages</span>
-                <hr className='border-[2px] border-white ' />
+                <span className='bg-gray-100 text-gray-400 text-[1.2rem] font-[500] px-[2rem] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  '>Messages</span>
+                <hr className='border-[1px] border-gray-300 ' />
             </div>
 
-            <div className="w-full flex grow bg-yellow-200 ">
+            <div className="w-full mb-[1rem] flex flex-col items-start gap-y-[1rem] grow overflow-y-scroll newScroll ">
+                <div className=" h-auto flex  flex-col gap-y-[0.2rem] px-[1rem] py-[0.5rem] rounded-[1rem] bg-white  ">
+                    <div className=" text-[1.4rem] font-[600] flex  ">Parth</div>
+                    <div className=" text-[1.3rem] font-[400] flex ">
+                        Lorem ipsum dolor sit, amet consectetur 
+                    </div>
+                </div>
 
+                <div className=" h-auto flex flex-col gap-y-[0.2rem] px-[1rem] py-[0.5rem] rounded-[1rem] bg-white  ">
+                    <div className=" text-[1.4rem] font-[600] flex  ">Parth</div>
+                    <div className=" text-[1.3rem] font-[400] flex ">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    </div>
+                </div>
+
+                <div className=" h-auto flex self-end flex-col gap-y-[0.2rem] px-[1rem] py-[0.5rem] rounded-[1rem] bg-white  ">
+                    <div className="text-[1.4rem] font-[600] flex justify-end  ">Parth</div>
+                    <div className="text-[1.3rem] font-[400] flex justify-end ">
+                        Lorem ipsum dolor sit amet consectetur
+                    </div>
+                </div>
+
+                <div className=" h-auto flex flex-col gap-y-[0.2rem] px-[1rem] py-[0.5rem] rounded-[1rem] bg-white  ">
+                    <div className=" text-[1.4rem] font-[600] flex  ">Parth</div>
+                    <div className=" text-[1.3rem] font-[400] flex ">
+                        Lorem ipsum dolor
+                    </div>
+                </div>
             </div>
 
-            <div className="w-full p-[0.5rem] flex bg-red-400 ">
-                <input className='h-[6vh] flex grow  ' type="text" placeholder='Write x x xa messagennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn' />
-                <button className='w-[3.5rem] h-full text-[2rem] rounded-[0.5rem] flex justify-center items-center bg-yellow-300 '><TiLocationArrow /></button>
+            <div className="w-full p-[0.5rem] flex bg-white rounded-[1rem] ">
+                <input className='h-[6vh] text-[1.4rem] px-[1rem] flex grow focus:outline-none  ' type="text" placeholder='Write a message' />
+                <button className='w-[3.5rem] h-full text-[2rem] rounded-[0.5rem] flex justify-center items-center bg-yellow-300  '><TiLocationArrow /></button>
             </div>
 
         </div >
--- src/Components/ConferencePage/ConferencePage.jsx
+++ src/Components/ConferencePage/ConferencePage.jsx
@@ -13,20 +13,20 @@ const ConferencePage = () => {
 
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center '>
-      <div className="w-full grow flex bg-red-200 ">
+      <div className="w-full h-[85vh] flex ">
         <div className="w-[70%] h-full bg-green-200 "></div>
 
-        <div className="grow h-full p-[1rem] flex bg-green-500 ">
+        <div className="w-[30%] h-full p-[1rem] flex  ">
           <Chatroom />
         </div>
       </div>
 
       <div className="w-full h-[15vh] flex justify-center items-center gap-x-[3rem] border-[1px] border-black ">
-        <button><FaVideo className='text-[2rem] bg-gray-200 w-auto h-auto px-[3rem] py-[1.2rem] rounded-[1rem] border-[1px] border-black ' /></button>
-        <button><AiFillAudio className='text-[2rem] bg-gray-200 w-auto h-auto px-[3rem] py-[1.2rem] rounded-[1rem] border-[1px] border-black ' /></button>
-        <button><MdCallEnd className='text-[2rem] bg-gray-200 w-auto h-auto px-[3rem] py-[1.2rem] rounded-[1rem] border-[1px] border-black ' /></button>
-        <button><AiOutlineFullscreen className='text-[2rem] bg-gray-200 w-auto h-auto px-[3rem] rounded-[1rem] py-[1.2rem] border-[1px] border-black ' /></button>
-        <button><AiOutlineEllipsis className='text-[2rem] bg-gray-200 w-auto h-auto px-[3rem] rounded-[1rem] py-[1.2rem] border-[1px] border-black ' /></button>
+        <button className='w-[7rem] h-[5rem] rounded-[0.5rem] text-[2.3rem] flex justify-center items-center bg-gray-300 '><FaVideo /></button>
+        <button className='w-[7rem] h-[5rem] rounded-[0.5rem] text-[2.3rem] flex justify-center items-center bg-gray-300 '><AiFillAudio /></button>
+        <button className='w-[7rem] h-[5rem] rounded-[0.5rem] text-[2.3rem] flex justify-center items-center bg-gray-300 '><MdCallEnd /></button>
+        <button className='w-[7rem] h-[5rem] rounded-[0.5rem] text-[2.3rem] flex justify-center items-center bg-gray-300 '><AiOutlineFullscreen /></button>
+        <button className='w-[7rem] h-[5rem] rounded-[0.5rem] text-[3.5rem] flex justify-center items-center bg-gray-300 '><AiOutlineEllipsis /></button>
       </div>
     </div>
   )
